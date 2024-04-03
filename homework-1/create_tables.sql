f42f71aeeae6e8b273dd257b4ab24bd1a6f53753
AnS12as <142240329+AnS12as@users.noreply.github.com>
2024-04-03 11:26:48 +0300

    homework. commited SQL

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+-- CREATE TABLE customers (
+--     customer_id VARCHAR(255) PRIMARY KEY,
+--     name VARCHAR(100),
+--     email VARCHAR(100),
+--     address VARCHAR(100),
+--     company_name VARCHAR(255),
+--     contact_name VARCHAR(255)
+-- );
+--
+-- CREATE TABLE orders (
+--     order_id SERIAL PRIMARY KEY,
+--     order_date DATE,
+--     customer_id VARCHAR(255),
+--     employee_id INT,
+--     ship_city VARCHAR(255),
+--     FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+--     FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
+-- );
+--
+-- SELECT * FROM employees;
+-- SELECT * FROM customers;
+-- SELECT * FROM orders;
