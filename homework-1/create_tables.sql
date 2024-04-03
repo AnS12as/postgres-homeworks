6b0ce83889c5b91a2a032a7585ce8e6b97cab44a
AnS12as <142240329+AnS12as@users.noreply.github.com>
2024-04-03 21:19:16 +0300

    added a table of employees

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,4 +1,16 @@
 -- SQL-команды для создания таблиц
+--
+--
+--     CREATE TABLE employees (
+--     employee_id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     title VARCHAR(255),
+--     birth_date DATE,
+--     notes TEXT
+-- );
+--
+--
 -- CREATE TABLE customers (
 --     customer_id VARCHAR(255) PRIMARY KEY,
 --     name VARCHAR(100),
@@ -7,6 +19,7 @@
 --     company_name VARCHAR(255),
 --     contact_name VARCHAR(255)
 -- );
+--
 --
 -- CREATE TABLE orders (
 --     order_id SERIAL PRIMARY KEY,
@@ -17,6 +30,8 @@
 --     FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
 --     FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
 -- );
+--
+--
 --
 -- SELECT * FROM employees;
 -- SELECT * FROM customers;
